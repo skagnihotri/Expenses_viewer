70305e887ad4ad03ff6ec6b46e520242c6b71093
Shubham Kumar <skagnihotri1@gmail.com>
2022-10-28 11:19:05 +0530

    Added state to change expense title.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "../../index.css";
 import Cards from "../UI/Cards";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!");
+  };
+
   return (
     <Cards className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click Me!</button>
     </Cards>
   );
 }
